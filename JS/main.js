1f51796228b801a59bf55147bd3c29e9b9bcd1eb
yanis.rezgui <yanis.rezgui57@gmail.com>
2022-10-19 14:20:58 +0200

    commit JS SHELL

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,34 @@
-
 let min = 1;
 let max = 100;
 let random = Math.floor(Math.random() * (max - min)) + min;
+let playerName = document.getElementById("name")
 let playerNumber = document.getElementById("input")
 let response = document.getElementById('response')
+let replay = document.getElementById('replay')
 let tryNumber = 0
 
 
 console.log("le chiffre aleatoire est : " + random)
 document.getElementById("input").addEventListener("keyup", function (e) {
-    if (e.keyCode === 13 && playerNumber.value != "") {
+    if (e.keyCode === 13 && playerNumber.value != "" && playerName.value != "") {
         tryNumber++
         console.log(playerNumber.value)
         if (playerNumber.value == random) {
             response.innerHTML = 'vous avez gagne en ' + tryNumber + ' coups'
+
+            const record = {
+                nom: playerName.value,
+                coups: tryNumber
+
+            }
+
+            let monStockage = localStorage;
+            localStorage.setItem('Record', JSON.stringify(record))
+            console.log(localStorage.getItem('Record'))
+
+            console.log(playerName.value)
+            console.log(tryNumber)
+
         } else if (playerNumber.value < random) {
             response.innerHTML = 'Le nombre est trop petit'
         } else if (playerNumber.value > random) {
@@ -23,6 +38,3 @@ document.getElementById("input").addEventListener("keyup", function (e) {
         playerNumber.focus
     }
 });
-
-
-
